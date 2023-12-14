5ddb6671f4677e708f2413c0c0479ce679ce2dce
Alina Nanu <nanualinav@gmail.com>
2023-12-14 12:28:51 +0200

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Container, Text, Button } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <ChakraProvider>
+    <Container 
+      alignItems="center" 
+      border="1px solid"
+      my="60px" p={5}>
+      <Text>Memory Game</Text>
+      <Button>Play</Button>
+    </Container>
+  </ChakraProvider>
+  ) 
 }
-
 export default App;
