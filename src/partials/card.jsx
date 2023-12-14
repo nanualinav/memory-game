bdb357c7e02f6bc080496e0e64ed5073f42a6ba7
Alina Nanu <nanualinav@gmail.com>
2023-12-14 17:42:32 +0200

    Implement Card component

---
+++ src/partials/card.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import {
+    Box,
+    Image,
+    Text
+} from '@chakra-ui/react'
+
+const Card = ({ card, handleChoice, isFlipped, disabled }) => {
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card)
+        }
+    }
+
+    return (
+        <Box
+            position='relative'
+            width='150px'
+            height='250px'
+            my={5} boxShadow='xl'>
+            <Box
+                position="absolute"
+                top="0" left="0"
+                transform={isFlipped ? "rotateY(0deg)" : "rotateY(90deg)"}
+                transition="all ease-in-out 0.5s"
+                transitionDelay={isFlipped ? "0.2s" : "0s"}
+            >
+                <Image
+                    src={card.src} alt="card"
+                    objectFit="contain">
+                </Image>
+                <Text
+                    align="center"
+                    my={3} color="gray.700"
+                >{card.name}
+                </Text>
+            </Box>
+            <Box
+                as="button"
+                width="150px" height="250px"
+                top="0" left="0"
+                transform={isFlipped ? "rotateY(90deg)" : "rotateY(0deg)"}
+                position="absolute"
+                transition="all ease-in-out 0.5s"
+                transitionDelay={isFlipped ? "0" : "0.2s"}
+                onClick={handleClick}
+            >
+                <Text
+                    as="samp"
+                    align="center"
+                    fontSize="3xl"
+                    color="gray.600"
+                >Card
+                </Text>
+            </Box>
+        </Box>
+    )
+}
+
+export default Card
